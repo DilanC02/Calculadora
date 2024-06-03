beb69bbf4ab4b0967331841e4efd1dc744873950
DilanC02 <dilank0508gmail.com>
2024-06-03 11:03:46 -0500

    Dividir

--- script.js
+++ script.js
@@ -24,3 +24,12 @@ function multiplicar() {
     const resultado = numero1 * numero2;
     mostrarResultado(resultado);
 }
+function dividir() {
+    const { numero1, numero2 } = obtenerValores();
+    if (numero2 === 0) {
+        mostrarResultado('Error: División por cero');
+    } else {
+        const resultado = numero1 / numero2;
+        mostrarResultado(resultado);
+    }
+}
